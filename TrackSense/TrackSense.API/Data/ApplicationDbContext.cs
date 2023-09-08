6ae786de77077ed10658fa7634c0f405557b8cc3
Binh Nguyen <tienbinhnguyen84@gmail.com>
2023-09-08 14:47:17 -0400

    définir primary key de tables CompletedRidePoint Et PlannedRidePoint

--- TrackSense/TrackSense.API/Data/ApplicationDbContext.cs
+++ TrackSense/TrackSense.API/Data/ApplicationDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using TrackSense.API.Entities;
 using TrackSense.API.Services.DTO;
 
 namespace TrackSense.API.Data
@@ -12,8 +11,18 @@ namespace TrackSense.API.Data
         public DbSet<AddressDTO> AddressDTOs { get; set; }
         public DbSet<PlannedRideDTO>PlannedRideDTOs { get; set; }
         public DbSet<PlannedRidePointDTO>PlannedRidePointDTOs { get; set; }
+      
         public DbSet<PlannedRideStatisticsDTO> PlannedRideStatisticsDTOs { get; set; }
         public DbSet<CompletedRideDTO> CompletedRideDTOs { get; set; }
+        public DbSet<CompletedRidePointDTO> CompletedRidePointDTOs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CompletedRidePointDTO>().HasKey(c => new { c.CompletedRideId, c.LocationId });
+            modelBuilder.Entity<PlannedRidePointDTO>().HasKey(c => new { c.PlannedRideId, c.LocationId });
+
+            base.OnModelCreating(modelBuilder);
+        }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
       
