420ecd79cdbb99f1bd0bfe4b8d3b619bae1dab3f
Binh <tienbinhnguyen84@gmail.com>
2023-10-19 14:27:42 -0400

    adjouter GET: api/Users/availibityLogin

---
+++ TrackSense/TrackSense.API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrackSense.API.Models;
+using TrackSense.API.Services.ServiceUsers;
+
+namespace TrackSense.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ManipulationUsers m_manipulationUsers;
+
+        public UsersController(ManipulationUsers manipulationUsers)
+        {
+            m_manipulationUsers = manipulationUsers;
+        }
+        // GET: api/Users/availibityLogin
+        [HttpGet("availibityLogin")]
+        public ActionResult<AvailibilityModel> GetAvailibityLogin(string p_userLogin)
+        {
+            bool result = m_manipulationUsers.UserExist(p_userLogin);
+
+            return Ok(new AvailibilityModel(result));
+        }
+
+
+    }
+}
---
+++ TrackSense/TrackSense.API/Models/AvailibilityModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TrackSense.API.Models
+{
+    public class AvailibilityModel
+    {
+        public bool IsAvailable { get; set; }
+
+        public AvailibilityModel(bool p_isAvailable)
+        {
+            IsAvailable = p_isAvailable;
+        }
+    }
+    
+}
